3500c200ffd2714771f31a4a186e937e04aa967b
Jsoriano <jsorianoh89@gmail.com>
2025-06-16 20:53:07 +0200

    Organizo los ejercicios en carpetas por temas: bucles, funciones, condicionales, etc.

---
+++ ejercicios-input/area-triangulo.py
@@ -0,0 +1,10 @@
+# medir el area de un triangulo
+# Pedir al usuario la base y la altura del triángulo
+base = float(input("Ingresa la medida de la base del triangulo: "))
+
+altura = float(input("Ingresa la Altura: "))
+
+
+area = base * altura /2
+
+print(f"El Area del Triangulo es: {area:2}")
---
+++ ejercicios-input/calculador-areas.py
@@ -0,0 +1,44 @@
+# Crea un pequeño programa en Python que:
+
+# 1️ Pregunte al usuario qué figura quiere calcular:
+
+# triángulo
+
+# cuadrado
+
+# rectángulo
+
+# círculo
+
+print("Calculador de Areas de Figuras!")
+
+print("Triangulo")
+print("Cuadrado")
+print("Rectangulo")
+print("Circulo")
+
+input("Que figura quieres medir? ")
+
+
+# 2️⃣ Según la figura elegida, pida los datos correspondientes:
+
+# Para el triángulo: base y altura.
+base = float(input("Ingresa la base del triangulo: "))
+
+altura = float(input("Ingresa la Altura: "))
+
+
+area = base * altura / 2
+
+print(f"El Area del Triangulo es {area}")
+
+# Para el cuadrado: lado.
+
+# Para el rectángulo: base y altura.
+
+# Para el círculo: radio.
+
+# 3️⃣ Calcule el área con la fórmula correcta.
+
+#4️⃣ Muestre el resultado al usuario.
+
