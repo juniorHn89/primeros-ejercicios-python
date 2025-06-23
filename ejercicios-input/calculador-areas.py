3bf3208629c3fde1b727e21a969fb9a649df252e
Jsoriano <jsorianoh89@gmail.com>
2025-06-23 20:57:01 +0200

    Actualizo cálculo de áreas en ejercicios-input

--- ejercicios-input/calculador-areas.py
+++ ejercicios-input/calculador-areas.py
@@ -33,10 +33,27 @@ area = base * altura / 2
 print(f"El Area del Triangulo es {area}")
 
 # Para el cuadrado: lado.
+lado = float(input("Ingresa el lado del cuadrado: "))
+
+area = lado * lado
 
 # Para el rectángulo: base y altura.
+base = float(input("Ingresa la base del Rectangulo: "))
 
+altura = float(input("Ingresa la Altura: "))
+
+
+area = base * altura 
+
+print(f"El Area del Rectangulo es {area}")
+
+import math
 # Para el círculo: radio.
+float(input("Ingresa el radio del circulo: "))
+
+area = radio * Pi 
+
+print(f"El Area del circulo es {area}")
 
 # 3️⃣ Calcule el área con la fórmula correcta.
 
